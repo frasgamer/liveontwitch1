4a5f5d2a95f041875316c2229e76e25163e7d2fc
frasgamer <44925264+frasgamer@users.noreply.github.com>
2018-12-18 21:46:44 +0300

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,6 +1,8 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
+
+
   client.user.setGame(`تم التويتش من قبل اخوك فراس`,'https://www.twitch.tv/By:A7MD');
   console.log('BOT ONLINE OP SHOP AND LAST COMUNITY y7bkm');
 });
